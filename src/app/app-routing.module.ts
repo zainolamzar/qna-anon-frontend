e6951e9efa7cec61a069006a13d4931c8f029c07
Zain Coder <zainolamzar.work@gmail.com>
2024-06-18 21:08:06 +0800

    first

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,16 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { MessageFormComponent } from './message-form/message-form.component';
 import { AdminLoginComponent } from './admin-login/admin-login.component';
 import { AdminDashboardComponent } from './admin-dashboard/admin-dashboard.component';
 import { AdminMessageDetailComponent } from './admin-message-detail/admin-message-detail.component';
-import { AuthGuard } from './services/auth/auth.guard';
 
 const routes: Routes = [
-  { path: '', redirectTo: '/message-form', pathMatch: 'full' },
+  { path: '', component: MessageFormComponent },
   { path: 'admin-login', component: AdminLoginComponent },
-  { path: 'admin-dashboard', component: AdminDashboardComponent, canActivate: [AuthGuard] },
-  { path: 'admin-message-detail/:id', component: AdminMessageDetailComponent, canActivate: [AuthGuard] },
-  { path: '**', redirectTo: '' }
+  { path: 'admin-dashboard', component: AdminDashboardComponent },
+  { path: 'admin-message-detail/:id', component: AdminMessageDetailComponent },
+  { path: '**', redirectTo: '' } // Fallback route
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
-import { HttpClientModule } from '@angular/common/http';
+import { HttpClientModule, provideHttpClient, withFetch } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
@@ -31,7 +31,8 @@ import { AuthGuard } from './services/auth/auth.guard';
   providers: [
     MessageService,
     AuthService,
-    AuthGuard
+    AuthGuard,
+    provideHttpClient(withFetch())
   ],
   bootstrap: [AppComponent]
 })

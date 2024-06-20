521f589a766521ef2b7b76b5add091e4eb384e1d
Zain Coder <zainolamzar.work@gmail.com>
2024-06-21 00:23:45 +0800

    fixed bug

--- src/app/services/message/message.service.ts
+++ src/app/services/message/message.service.ts
@@ -1,6 +1,8 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { HttpClient, HttpHeaders, HttpErrorResponse } from '@angular/common/http';
+import { Observable, throwError } from 'rxjs';
+import { catchError } from 'rxjs/operators';
+import { SnackbarService } from '../snackbar/snackbar.service'; // Import your SnackbarService
 
 @Injectable({
   providedIn: 'root'
@@ -8,17 +10,28 @@ import { Observable } from 'rxjs';
 export class MessageService {
   private apiUrl = 'https://qna-anonymous-server.onrender.com/api/messages';
 
-  constructor(private http: HttpClient) { }
+  constructor(
+    private http: HttpClient,
+    private snackbarService: SnackbarService // Inject SnackbarService
+  ) { }
 
-  submitMessage(message: string): Observable<any> {
-    return this.http.post<any>(this.apiUrl, { content: message });
-  }
+  submitMessage(messageData: any): Observable<any> {
+    const httpOptions = {
+      headers: new HttpHeaders({
+        'Content-Type': 'application/json'
+      })
+    };
 
-  getMessages(): Observable<any[]> {
-    return this.http.get<any[]>(this.apiUrl);
-  }
-
-  getMessageById(id: string): Observable<any>{
-    return this.http.get<any>(`${this.apiUrl}/${id}`);
+    return this.http.post<any>(this.apiUrl, { messageContent: messageData }, httpOptions)
+      .pipe(
+        catchError((error: HttpErrorResponse) => {
+          if (error.status === 400 && error.error.message.includes('duplicate key error')) {
+            this.snackbarService.showError('Message with this ID already exists.');
+          } else {
+            this.snackbarService.showError('Failed to submit message.');
+          }
+          return throwError('Error submitting message.');
+        })
+      );
   }
 }

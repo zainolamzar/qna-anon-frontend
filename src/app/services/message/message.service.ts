3e62d71692150d1297eb613c10205d2da96b286d
Zain Coder <zainolamzar.work@gmail.com>
2024-06-21 00:33:36 +0800

    rollback

--- src/app/services/message/message.service.ts
+++ src/app/services/message/message.service.ts
@@ -1,8 +1,6 @@
 import { Injectable } from '@angular/core';
-import { HttpClient, HttpHeaders, HttpErrorResponse } from '@angular/common/http';
-import { Observable, throwError } from 'rxjs';
-import { catchError } from 'rxjs/operators';
-import { SnackbarService } from '../snackbar/snackbar.service'; // Import your SnackbarService
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -10,28 +8,17 @@ import { SnackbarService } from '../snackbar/snackbar.service'; // Import your S
 export class MessageService {
   private apiUrl = 'https://qna-anonymous-server.onrender.com/api/messages';
 
-  constructor(
-    private http: HttpClient,
-    private snackbarService: SnackbarService // Inject SnackbarService
-  ) { }
+  constructor(private http: HttpClient) { }
 
-  submitMessage(messageData: any): Observable<any> {
-    const httpOptions = {
-      headers: new HttpHeaders({
-        'Content-Type': 'application/json'
-      })
-    };
+  submitMessage(message: string): Observable<any> {
+    return this.http.post<any>(this.apiUrl, { content: message });
+  }
 
-    return this.http.post<any>(this.apiUrl, { messageContent: messageData }, httpOptions)
-      .pipe(
-        catchError((error: HttpErrorResponse) => {
-          if (error.status === 400 && error.error.message.includes('duplicate key error')) {
-            this.snackbarService.showError('Message with this ID already exists.');
-          } else {
-            this.snackbarService.showError('Failed to submit message.');
-          }
-          return throwError('Error submitting message.');
-        })
-      );
+  getMessages(): Observable<any[]> {
+    return this.http.get<any[]>(this.apiUrl);
+  }
+
+  getMessageById(id: string): Observable<any>{
+    return this.http.get<any>(`${this.apiUrl}/${id}`);
   }
 }

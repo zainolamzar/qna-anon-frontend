f9ea5c814f194e90da68701d3ce1d2eca4582c28
Zain Coder <zainolamzar.work@gmail.com>
2025-09-04 20:45:02 +0800

    fixed

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,11 +2,12 @@ import { Routes } from '@angular/router';
 import { AdminDashboardComponent } from './admin-dashboard/admin-dashboard.component';
 import { AdminLoginComponent } from './admin-login/admin-login.component';
 import { AdminMessageDetailComponent } from './admin-message-detail/admin-message-detail.component';
+import { MessageFormComponent } from './message-form/message-form.component';
 
 export const routes: Routes = [
+  { path: '', component: MessageFormComponent }, // root (no slash)
   { path: 'admin-login', component: AdminLoginComponent },
   { path: 'admin-dashboard', component: AdminDashboardComponent },
   { path: 'admin-message-detail/:id', component: AdminMessageDetailComponent },
-  { path: '', redirectTo: '/admin-login', pathMatch: 'full' }, // default route
-  { path: '**', redirectTo: '/admin-login' }, // catch-all fallback
+  { path: '**', redirectTo: 'admin-login' }, // wildcard fallback
 ];
